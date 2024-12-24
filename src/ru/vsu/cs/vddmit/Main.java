48cdb7894e887fb0d105f3157cf2106a5689071b
vddmit <danvoropaeff@yandex.ru>
2024-12-24 23:15:06 +0300

    RE-theme

--- src/ru/vsu/cs/vddmit/Main.java
+++ src/ru/vsu/cs/vddmit/Main.java
@@ -6,53 +6,53 @@ import java.text.SimpleDateFormat;
 public class Main {
 
     public static void main(String[] args) throws ParseException {
-        Faculty aeroastro = new Faculty("Aeronautics and Astronautics");
-        Institute aero = new Institute("aeronautics", "University square, 1");
-        Institute astro = new Institute("astronautics", "University square, 1");
-        aeroastro.setInstitutes(new Institute[]{aero, astro});
+        Faculty engineering = new Faculty("Engineering Sciences");
+        Institute robotics = new Institute("Robotics Research Center", "Tech Avenue, 42");
+        Institute aiLab = new Institute("Artificial Intelligence Lab", "Tech Avenue, 45");
+        engineering.setInstitutes(new Institute[]{robotics, aiLab});
 
-        ResearchAssociate employee1 = new ResearchAssociate(1, "John Doe", "jodo@gmail.com", "aero");
-        ResearchAssociate employee2 = new ResearchAssociate(2, "Ronald Roe", "roro.com", "aero");
-        ResearchAssociate employee3 = new ResearchAssociate(3, "Wonnie Woah", "wowo@gmail.com", "astro");
-        ResearchAssociate employee4 = new ResearchAssociate(4, "Eustas Eum", "eueu@gmail.com", "astro");
-        aero.setEmployees(new ResearchAssociate[]{employee1, employee2});
-        astro.setEmployees(new ResearchAssociate[]{employee3, employee4});
+        ResearchAssociate researcher1 = new ResearchAssociate(1, "Alice Smith", "asmith@gmail.com", "robotics");
+        ResearchAssociate researcher2 = new ResearchAssociate(2, "Bob Johnson", "bobj@gmail.com", "robotics");
+        ResearchAssociate researcher3 = new ResearchAssociate(3, "Clara Brown", "clara@gmail.com", "ai");
+        ResearchAssociate researcher4 = new ResearchAssociate(4, "David Lee", "dlee@gmail.com", "ai");
+        robotics.setEmployees(new ResearchAssociate[]{researcher1, researcher2});
+        aiLab.setEmployees(new ResearchAssociate[]{researcher3, researcher4});
 
-        Project project1 = new Project("Railgun",
-                new SimpleDateFormat("dd.MM.yyyy").parse("12.12.2023"),
-                new SimpleDateFormat("dd.MM.yyyy").parse("08.03.2024"), new Participation(5));
-        employee1.setProjects(new Project[]{project1});
+        Project project1 = new Project("Autonomous Drones",
+                new SimpleDateFormat("dd.MM.yyyy").parse("01.01.2024"),
+                new SimpleDateFormat("dd.MM.yyyy").parse("30.06.2024"), new Participation(8));
+        researcher1.setProjects(new Project[]{project1});
 
-        Course course1 = new Course("Cosmic rays", 25, 60);
-        Lecturer lecturer1 = new Lecturer(5, "Donnie Duh", "dodu@gmail.com", "astro");
+        Course course1 = new Course("Neural Networks", 40, 75);
+        Lecturer lecturer1 = new Lecturer(5, "Eleanor Green", "egreen@gmail.com", "ai");
         lecturer1.setCourses(new Course[]{course1});
 
         AdministrativeEmployee administrativeEmployee = new AdministrativeEmployee(6,
-                "Sonny Soy", "soso@gmail.com", "admin");
+                "Frank Moore", "fmoore@gmail.com", "admin");
 
-        Institute[] institutes = aeroastro.getInstitutes();
-        System.out.println("Institutes on " + aeroastro.getName() + ": \n");
+        Institute[] institutes = engineering.getInstitutes();
+        System.out.println("Institutes in " + engineering.getName() + ": \n");
         for (Institute institute : institutes) {
             System.out.println(institute.getName());
         }
         System.out.println();
 
-        ResearchAssociate[] researchEconomy = aero.getEmployees();
-        System.out.println("Research Associates on " + aero.getName() + ": \n");
-        for (ResearchAssociate researchAssociate : researchEconomy) {
+        ResearchAssociate[] roboticsResearchers = robotics.getEmployees();
+        System.out.println("Research Associates in " + robotics.getName() + ": \n");
+        for (ResearchAssociate researchAssociate : roboticsResearchers) {
             System.out.println(researchAssociate.getName());
         }
         System.out.println();
 
-        ResearchAssociate[] researchFinance = astro.getEmployees();
-        System.out.println("Research Associates on " + astro.getName() + ": \n");
-        for (ResearchAssociate researchAssociate : researchFinance) {
+        ResearchAssociate[] aiResearchers = aiLab.getEmployees();
+        System.out.println("Research Associates in " + aiLab.getName() + ": \n");
+        for (ResearchAssociate researchAssociate : aiResearchers) {
             System.out.println(researchAssociate.getName());
         }
         System.out.println();
 
-        Project[] projects = employee1.getProjects();
-        System.out.println("Projects of " + employee1.getName() + ": \n");
+        Project[] projects = researcher1.getProjects();
+        System.out.println("Projects of " + researcher1.getName() + ": \n");
         for (Project project : projects) {
             System.out.println(project.getName());
         }
@@ -64,7 +64,8 @@ public class Main {
             System.out.println(course.getName());
         }
         System.out.println();
-        System.out.println("Admin:");
+
+        System.out.println("Administrative Staff:");
         System.out.println();
         System.out.println(administrativeEmployee.getName());
     }
